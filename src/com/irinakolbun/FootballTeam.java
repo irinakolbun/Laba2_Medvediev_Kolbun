4db5cb22817d63b1fcff134321fdef66e5778202
irinakolbun <kolbunirina01@gmail.com>
2020-02-19 12:55:43 +0200

    Started a project (Initialized FootballTeam class)

---
+++ src/com/irinakolbun/FootballTeam.java
@@ -0,0 +1,28 @@
+package com.irinakolbun;
+
+public class FootballTeam {
+    private String teamName;
+    //private int[][] scores = new int[10][2];
+    private int teamTotalScore = 0;
+
+    FootballTeam(String inputData) {
+        parseData(inputData);
+
+    }
+
+    private void parseData(String inputData) {
+        String[] data = inputData.split(",");
+        this.teamName = data[0];
+        for (int i = 1; i < data.length; i++) {
+            String[] score = data[i].split(":");
+            int teamScore = Integer.parseInt(score[0]);
+            int opponentScore = Integer.parseInt(score[0]);
+            if(teamScore > opponentScore){
+                teamTotalScore += 3;
+            }
+            else if(teamScore == opponentScore){
+                teamTotalScore += 1;
+            }
+        }
+    }
+}
---
+++ src/com/irinakolbun/Main.java
@@ -0,0 +1,20 @@
+package com.irinakolbun;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String csvFile = "inputs/file1.csv";
+        String line = "";
+        String cvsSplitBy = ",";
+        //List<List<String>> records = new ArrayList<>();
+
+
+
+    }
+
+}
