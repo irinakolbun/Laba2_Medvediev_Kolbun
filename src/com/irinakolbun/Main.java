490229ff53a4e5fd2b6b58b2ac8d925fcc00a3da
MPH <misha.medvedev2001@gmail.com>
2020-02-19 22:59:04 +0200

    Added function to write results to the file

--- src/com/irinakolbun/Main.java
+++ src/com/irinakolbun/Main.java
@@ -1,15 +1,19 @@
 package com.irinakolbun;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         String csvFilePath = "inputs/file1.csv";
         FileParser parser = new FileParser(csvFilePath);
         FootballTeam[] footballTeams = parser.getFootballTeamsFromFile();
+
         String text = getFinalScoresTable(footballTeams);
-        System.out.println(text);
+        writeResultsFile(text);
     }
 
     private static String getFinalScoresTable(FootballTeam[] teams) {
@@ -19,6 +23,13 @@ public class Main {
             outputText += (i + 1) + " place: " + teams[i].getTeamName() + " with score " + teams[i].getTeamTotalScore() + "\n";
         }
         return outputText;
+    }
+
+    private static void writeResultsFile(String text) throws IOException {
+        File file = new File("outputs/results.csv");
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write(text);
+        fileWriter.close();
     }
 
 }
